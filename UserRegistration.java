359ecd1bf611aefb46864988bf44f861f34d6286
kunalkale98 <kunal,k2898@gmail.com>
2021-03-01 14:18:02 +0530

    Added: Regex for Last Name

--- UserRegistration.java
+++ UserRegistration.java
@@ -6,8 +6,8 @@ public class UserRegistration {
 	public static void firstName(Scanner sc) {
 		System.out.print("First Name: ");
 		String checkFName = sc.nextLine();
-		boolean b=Pattern.compile("^[A-Z]{1}[a-z]{2}$").matcher(checkFName).matches();
-		if(b == true) {
+		boolean fn = Pattern.compile("^[A-Z]{1}[a-z]{2}$").matcher(checkFName).matches();
+		if(fn == true) {
 			String firstName = checkFName;
 			System.out.println("First Name is "+firstName);
 		}
@@ -16,9 +16,23 @@ public class UserRegistration {
 		}
 	}
 
+	 public static void lastName(Scanner sc) {
+      System.out.print("Last Name: ");
+      String checkLName = sc.nextLine();
+      boolean ln = Pattern.compile("^[A-Z]{1}[a-z]{2}$").matcher(checkLName).matches();
+      if(ln == true) {
+         String lastName = checkLName;
+         System.out.println("Last Name is "+lastName);
+      }
+      else {
+         System.out.println("Invalid Last Name");
+      }
+   }
+
 	public static void main(String args[]){
 		Scanner sc = new Scanner(System.in);
 		firstName(sc);
+		lastName(sc);
 	}
 }
 
