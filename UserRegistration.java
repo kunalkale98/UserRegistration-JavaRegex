a832c1b57cf700c0b4d193fa853a7dc259fbfb34
kunalkale98 <kunal,k2898@gmail.com>
2021-03-01 18:37:34 +0530

    Added: Password validation for atleast one symbol

--- UserRegistration.java
+++ UserRegistration.java
@@ -3,78 +3,83 @@ import java.util.regex.Pattern;
 
 public class UserRegistration {
 
-	public static void firstName(Scanner sc) {
+	public static String firstName(Scanner sc) {
 		System.out.print("First Name: ");
-		String checkFName = sc.nextLine();
-		boolean fn = Pattern.compile("^[A-Z]{1}[a-z]{2,}$").matcher(checkFName).matches();
+		String firstName = sc.nextLine();
+		boolean fn = Pattern.compile("^[A-Z]{1}[a-z]{2,}$").matcher(firstName).matches();
 		if(fn == true) {
-			String firstName = checkFName;
-			System.out.println("First Name is "+firstName);
+			return firstName;
 		}
-		else {
+		else{
 			System.out.println("Invalid First Name");
+			return firstName(sc);
 		}
 	}
 
-	 public static void lastName(Scanner sc) {
-      System.out.print("Last Name: ");
-      String checkLName = sc.nextLine();
-      boolean ln = Pattern.compile("^[A-Z]{1}[a-z]{2,}$").matcher(checkLName).matches();
+	public static String lastName(Scanner sc) {
+     	System.out.print("Last Name: ");
+      String lastName = sc.nextLine();
+      boolean ln = Pattern.compile("^[A-Z]{1}[a-z]{2,}$").matcher(lastName).matches();
       if(ln == true) {
-         String lastName = checkLName;
-         System.out.println("Last Name is "+lastName);
-      }
-      else {
+			return lastName;
+		}
+		else{
          System.out.println("Invalid Last Name");
+			return lastName(sc);
       }
    }
 
-	 public static void email(Scanner sc) {
+	public static String email(Scanner sc) {
       System.out.print("Email: ");
-      String checkEmail = sc.nextLine();
-      boolean eml = Pattern.compile("^abc.[a-z]{1,}@bl.co.[a-z]{2,3}$").matcher(checkEmail).matches();
+      String email = sc.nextLine();
+      boolean eml = Pattern.compile("^abc.[a-z]{1,}@bl.co.[a-z]{2,3}$").matcher(email).matches();
       if(eml == true) {
-         String email = checkEmail;
-         System.out.println("Email Address is "+email);
-      }
-      else {
+			return email;
+		}
+		else{
          System.out.println("Invalid Email Address");
+			return email(sc);
       }
    }
 
-	 public static void phoneNo(Scanner sc) {
+	public static String phoneNo(Scanner sc) {
       System.out.print("Phone No: ");
-      String checkPhoneNo = sc.nextLine();
-      boolean pn = Pattern.compile("^[0-9]{1,3} [0-9]{10}$").matcher(checkPhoneNo).matches();
+      String phoneNo = sc.nextLine();
+      boolean pn = Pattern.compile("^[0-9]{1,3} [0-9]{10}$").matcher(phoneNo).matches();
       if(pn == true) {
-         String phoneNo = checkPhoneNo;
-         System.out.println("Phone No is "+phoneNo);
-      }
-      else {
+			return phoneNo;
+		}
+		else{
          System.out.println("Invalid Phone No");
+			return phoneNo(sc);
       }
    }
 
-	 public static void password(Scanner sc) {
+	public static String password(Scanner sc) {
       System.out.print("Password: ");
-      String checkPass = sc.nextLine();
-      boolean pass = Pattern.compile("(?=.*[A-Z])(?=.*[0-9])[a-zA-Z0-9]{8,}").matcher(checkPass).matches();
+      String password = sc.nextLine();
+      boolean pass = Pattern.compile("(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*?.,])[a-zA-Z0-9!@#$%^&*?.,]{8,}").matcher(password).matches();
       if(pass == true) {
-         String password = checkPass;
-         System.out.println("Password is "+password);
-      }
-      else {
+			return password;
+		}
+		else{
          System.out.println("Invalid Password");
+			return password(sc);
       }
    }
+
+	public static void toPrint(String fName, String lName, String email, String pNo, String pass){
+		System.out.println(fName+" , "+lName+" , "+email+" , "+pNo+" , "+pass);
+	}
 
 	public static void main(String args[]){
 		Scanner sc = new Scanner(System.in);
-		firstName(sc);
-		lastName(sc);
-		email(sc);
-		phoneNo(sc);
-		password(sc);
+		String fName = firstName(sc);
+		String lName = lastName(sc);
+		String email = email(sc);
+		String phoneNo = phoneNo(sc);
+		String pass = password(sc);
+		toPrint(fName,lName,email,phoneNo,pass);
 	}
 }
 
